d0c7040c01d68d3602c04c01635c67d3747d13bf
benbenbenof <92963597+benbenbenof@users.noreply.github.com>
2023-06-11 15:47:40 -0500

    Explain
    Explaining how to do stuff

---
+++ ExplainingEverything.bat
@@ -0,0 +1,5 @@
+REM So we set a variable using the set /a and letting the player decide the parameters of the variable
+REm we use goto to call a variable action (:a)
+Rem echo is like print function in python
+rem title is title
+rem that should explain something
